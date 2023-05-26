d944580ce5b1fda9c56e4884f341e1c35e385a2f
EduOC1022 <112728180+EduOC1022@users.noreply.github.com>
2023-05-26 13:52:53 -0300

    Atualização

--- SimonGame/js/index.js
+++ SimonGame/js/index.js
@@ -1,12 +1,46 @@
+// ordem em que a máquina vai acender as luzes
 let order = [];
 let playerOrder = [];
-let flash;
-let turn;
+let piscar;
+let turno;
 let good;
 let compTurn;
-let intervalId;
+let idIntervalo;
 let strict = false;
-let noise = true;
+let som = true;
 let on = false;
 let win;
 
+const contadorTurno = document.querySelector("#turno");
+const superiorEsquerdo = document.querySelector("#superior-esquerdo");
+const superiorDireito = document.querySelector("#superior-direito");
+const inferiorEsquerdo = document.querySelector("#inferior-esquerdo");
+const inferiorDireito = document.querySelector("#inferior-direito");
+const botaoStrict = document.querySelector("#strict");
+const botaoOn = document.querySelector("#on");
+const botaoStart = document.querySelector("#start");
+
+
+botaoStrict.addEventListener("click", (event) => {
+    if (botaoStrict.checked == true) {
+        strict = true;
+    } else {strict = false;}
+});
+
+botaoOn.addEventListener("click", (event) => {
+    if (botaoOn.checked == true) {
+        on = true;
+        contadorTurno.innerHTML = "-"
+    } else {
+        on = false;
+        contadorTurno.innerHTML = "";
+        clearColor();
+        clearInterval(idIntervalo);
+    }
+});
+
+botaoStart.addEventListener("click", (event) => {
+    if (on || win) {
+        
+    }
+});
