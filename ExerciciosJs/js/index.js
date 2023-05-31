c31688e85f761de8e484583d7e71c8511c33b7b2
EduOC1022 <112728180+EduOC1022@users.noreply.github.com>
2023-05-31 13:12:23 -0300

    att

--- ExerciciosJs/js/index.js
+++ ExerciciosJs/js/index.js
@@ -1,29 +1,30 @@
 // Exercício 1
-let anum1 = 3
-let anum2 = 12
+let aNum1 = 3
+let aNum2 = 1
 
 const numero1 = document.getElementById("anum1");
 const numero2 = document.getElementById("anum2");
 const ex1 = document.getElementById("saida1");
 
 function soma() {
-    return anum1 + anum2;
+    return aNum1 + aNum2;
 };
 
 let resultado1 = soma();
 
-numero1.innerHTML = anum1;
-numero2.innerHTML = anum2;
+numero1.innerHTML = aNum1;
+numero2.innerHTML = aNum2;
 ex1.innerHTML = resultado1;
 console.log(resultado1);
 
+
 // Exercício 2
-let bnum1 = 8;
+let bNum = 8;
 
 const ex2 = document.getElementById("saida2");
 
 function parouimpar() {
-    if (bnum1 % 2 == 0) {
+    if (bNum % 2 == 0) {
         return true;
     } else {
         return false;
@@ -35,6 +36,37 @@ ex2.innerHTML = resultado2;
 console.log(resultado2)
 
 
+// Exercício 3
+let cStringEntrada = "Eduardo Coelho";
+
+const cString = document.getElementById("cstring");
+const ex3 = document.getElementById("saida3");
+
+function contagem3() {
+    return cStringEntrada.length;
+}
+
+let resultado3 = contagem3();
+cString.innerHTML = cStringEntrada;
+ex3.innerHTML = resultado3;
+console.log(resultado3);
 
 
+// Exercício 4
+let dStringEntrada = "Eduardo Coelho";
+
+const ex4 = document.getElementById("saida4");
+const dString = document.getElementById("dstring");
+
+function inversao() {
+    return dStringEntrada.split("").reverse().join("");
+}
+
+let resultado4 = inversao();
+dString.innerHTML = dStringEntrada;
+ex4.innerHTML = resultado4;
+console.log(resultado3);
+
+
+// Exercício 5
 
