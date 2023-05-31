6edcc0b619b2c1a2ea7871664fa59546b78f37cd
EduOC1022 <112728180+EduOC1022@users.noreply.github.com>
2023-05-31 17:54:09 -0300

    att

--- ExerciciosJs/js/index.js
+++ ExerciciosJs/js/index.js
@@ -19,59 +19,54 @@
 
 
 // Exercício 2
-// let bNum = 8;
+let bNum = 8;
+const bNumOriginal = toString(bNum);
 
-// const ex2 = document.getElementById("saida2");
+function parouimpar() {
+    if (bNum % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-// function parouimpar() {
-//     if (bNum % 2 == 0) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
-
-// let resultado2 = parouimpar();
-// ex2.innerHTML = resultado2;
-// console.log(resultado2)
+let resultado2 = parouimpar();
+document.getElementById("bentradahtml").innerHTML = bNumOriginal;
+document.getElementById("saida2").innerHTML = resultado2;
+console.log(resultado2)
 
 
 // Exercício 3
 // let cStringEntrada = "Eduardo Coelho";
-
-// const cString = document.getElementById("cstring");
-// const ex3 = document.getElementById("saida3");
 
 // function contagem3() {
 //     return cStringEntrada.length;
 // }
 
 // let resultado3 = contagem3();
-// cString.innerHTML = cStringEntrada;
-// ex3.innerHTML = resultado3;
+
+// document.getElementById("cstring").innerHTML = cStringEntrada;
+// document.getElementById("saida3").innerHTML = resultado3;
 // console.log(resultado3);
 
 
 // Exercício 4
 // let dStringEntrada = "Eduardo Coelho";
 
-// const ex4 = document.getElementById("saida4");
-// const dString = document.getElementById("dstring");
-
 // function inversao() {
 //     return dStringEntrada.split("").reverse().join("");
 // }
 
 // let resultado4 = inversao();
-// dString.innerHTML = dStringEntrada;
-// ex4.innerHTML = resultado4;
-// console.log(resultado3);
+
+// document.getElementById("dstring").innerHTML = dStringEntrada;
+// document.getElementById("saida4").innerHTML = resultado4;
+// console.log(resultado4);
 
 
 // Exercício 5
-// const elistaNumeros = [1, 4, 6, 10, 3, 50]
-// const eArray = document.getElementById("earray");
-// const ex5 = document.getElementById("saida5");
+// const elistaNumeros = [1, 4, 6, 10, 3, 50];
+// const elistaNumerosOriginal = [...elistaNumeros]
 
 // function maiorNumero() {
 //     elistaNumeros.sort(function(a, b) {return b-a;});
@@ -79,28 +74,26 @@
 // }
 
 // let resultado5 = maiorNumero();
-// ex5.innerHTML = resultado5;
+
+// document.getElementById("fstring").innerHTML = elistaNumerosOriginal;
+// document.getElementById("saida5").innerHTML = resultado5;
 
 
 // Exercício 6
 
-const flistaNumeros = [1, 4, 6, 10, 3, 50]
-const flistaNumeros2 = flistaNumeros.map(mediaNumero);
-const fArray = document.getElementById("farray");
-const ex6 = document.getElementById("saida6");
+// const flistaNumeros = [20, 4, 10, 10, 3, 50]
+// let soma = 0
 
-function mediaNumero(num) {
-    return num + num;
-}
-
-let resultado6 = mediaNumero();
-ex6.innerHTML = resultado6;
-
-// const numbers = [65, 44, 12, 4];
-// const newArr = numbers.map(myFunction);
-
-// document.getElementById("demo").innerHTML = newArr;
-
-// function myFunction(num) {
-//   return num * 10;
+// function mediaNumero(i) {
+//     for(var i = 0; i < flistaNumeros.length; i++) {
+//         soma += flistaNumeros[i];
+//     }
+//     return soma;
 // }
+
+// let resultado6 = mediaNumero();
+
+// const fArray = document.getElementById("farray");
+
+// document.getElementById("fstring").innerHTML = flistaNumeros;
+// document.getElementById("saida6").innerHTML = resultado6;
