d10589c5f471bf6fb2c880873bcf4c1e99572024
EduOC1022 <112728180+EduOC1022@users.noreply.github.com>
2023-06-07 16:16:48 -0300

    atualização

--- ExerciciosJs-2/js/index.js
+++ ExerciciosJs-2/js/index.js
@@ -9,20 +9,64 @@ valor seja inválido e continue pedindo até que o usuário informe um valor vá
 //     }
 // }
 
-/* Exercício 2 - . Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual 
+/* Exercício 2 - Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual 
 ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.*/
+// function cadastro(){
+//     let usuario = document.getElementById("usuario").value;
+//     let senha = document.getElementById("senha").value;
+//     if (usuario == senha) {
+//         document.getElementById("usuario").value = "";
+//         document.getElementById("senha").value = "";
+//         document.getElementById("resposta2").innerHTML = "O usuário e a senha não podem ser iguais." + "<br>" + "Tente nevamente";
+//         return false
+//     } else {
+//         document.getElementById("usuario").value = "";
+//         document.getElementById("senha").value = "";
+//         document.getElementById("resposta2").innerHTML = "Registrado";
+//         return true 
+//     }
+// }
+
+
+/* Exercício 3 - Faça um programa que leia e valide as seguintes informações: 
+a. Nome: maior que 3 caracteres; 
+b. Idade: entre 0 e 150; 
+c. Salário: maior que zero; 
+d. Sexo: 'f' ou 'm'; 
+e. Estado Civil: 's', 'c', 'v', 'd';*/
+// const nome = document.getElementById('nome').value;
+// const idade = document.getElementById('idade').value;
+// const salario = document.getElementById('salario').value;
+// const sexo = document.getElementById('sexo').value;
+// const estadoCivil = document.getElementById('estaocivil').value;
+
+// function validacaogeral() {
+//     switch () {
+
+//     }
+// }
+
 
 /* Exercício 13 - Faça um programa que peça dois números, base e expoente, calcule e mostre o primeiro 
 número elevado ao segundo número. Não utilize a função de potência da linguagem. */
+// function potencia() {
+//     let base = document.getElementById("13base").value;
+//     let expoente = document.getElementById("13expoente").value;
+//     let operador = 1;
+
+//     for (i=1; i<=expoente; i++) {
+//         operador = operador * base;
+//     }
+//     document.getElementById("resposta13").innerHTML = operador;
+// }
+
 
 /*Exercício 14 - A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,... Faça um 
 programa capaz de gerar a série até o n−ésimo termo.*/
-
-
 function fibonacci() {
     let termo1 = 1;
     let termo2 = 1;
-    let termo3 = termo1 + termo2;
+    let termox = termo1 + termo2;
     let enezimo = document.getElementById("14num").value;                                                                     
     if (enezimo == 1) {
         termo1 = termo1.toString()
@@ -33,46 +77,27 @@ function fibonacci() {
         termo1 = termo1.toString()
         termo2 = termo2.toString()
         console.log(termo1 + " " +termo2);
-        document.getElementById("resultado14").innerHTML = termo1 + ", " +termo2;
+        document.getElementById("resultado14").innerHTML = termo1 + "<br>" +termo2;
     }
     if (enezimo == 3) {
         termo1 = termo1.toString()
         termo2 = termo2.toString()
-        termo3 = termo3.toString()
-        console.log(termo1 + " " + termo2 + " " + termo3);
-        document.getElementById("resultado14").innerHTML = termo1 + ", " + termo2 + ", " + termo3;
+        termox = termox.toString()
+        console.log(termo1 + " " + termo2 + " " + termox);
+        document.getElementById("resultado14").innerHTML = termo1 + "<br>" + termo2 + "<br>" + termox;
     }
     if (enezimo > 3) {
-        const resultado14 = [termo1, termo2, termo3]
+        const resultado14 = [termo1, termo2, termox]
         for (i = 3; i < enezimo; i++) {
             termo1 = termo2
-            termo2 = termo3;
-            termo3 = termo1 + termo2;
-            resultado14.push(termo3);
+            termo2 = termox;
+            termox = termo1 + termo2;
+            resultado14.push(termox);
             
             
-            console.log(termo3);
-            document.getElementById("resultado14").innerHTML = resultado14;
+            console.log(termox);
+            document.getElementById("resultado14").innerHTML = resultado14.join("<br>");
         }
     }  
 }
-    
-// let resultado = fibonacci()
-// document.getElementById('resposta14').value = resultado;
-// console.log(resultado)
 
-// Fn = fn-1 + Fn-2
-
-/*function numExpoente(num1, num2){
-
-    let base = parseInt(document.getElementById(num1).value);
-    let expoente = parseInt(document.getElementById(num2).value);
-    let numF = 1;
-  
-   for(i=1; i<=expoente; i++){
-    numF*= base;
-  }
-  
-    document.getElementById("resultado13").innerHTML = numF;
-  
-  }*/
--- ExerciciosJs/js/index.js
+++ ExerciciosJs/js/index.js
@@ -1,165 +1,173 @@
 // Exercício 1
-let aNum1 = 0
-let aNum2 = 0
+// let aNum1 = 0
+// let aNum2 = 0
 
+// function soma() {
+//     let aNum1 = 14
+//     let aNum2 = 12
+//     document.getElementById("anum1").innerHTML = aNum1;
+//     document.getElementById("anum2").innerHTML = aNum2;
+//     return aNum1 + aNum2;
+// };
+
+// let resultado1 = soma();
+
+// document.getElementById("saida1").innerHTML = resultado1;
+// console.log(resultado1);
+
+// Exercício 1
 function soma() {
-    let aNum1 = 14
-    let aNum2 = 12
-    document.getElementById("anum1").innerHTML = aNum1;
-    document.getElementById("anum2").innerHTML = aNum2;
-    return aNum1 + aNum2;
+    let aNum1 = document.getElementById("anum1").value;
+    let aNum2 = document.getElementById("anum2").value;
+    document.getElementById("saida1").innerHTML = aNum1 + aNum2;
+    console.log(aNum1 + aNum2);
 };
-
-let resultado1 = soma();
-
-document.getElementById("saida1").innerHTML = resultado1;
-console.log(resultado1);
 
 
 // Exercício 2
 
-function parouimpar() {
-    let bNum = 20;
-    if (bNum % 2 == 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
+// function parouimpar() {
+//     let bNum = 20;
+//     if (bNum % 2 == 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
-let resultado2 = parouimpar();
+// let resultado2 = parouimpar();
 
-document.getElementById("saida2").innerHTML = resultado2;
-console.log(resultado2)
+// document.getElementById("saida2").innerHTML = resultado2;
+// console.log(resultado2)
 
 
 // Exercício 3
-let cStringEntrada = "";
+// let cStringEntrada = "";
 
-function contagem3() {
-    let cStringEntrada = "Eduardo Coelho";
-    document.getElementById("cstring").innerHTML = cStringEntrada;
-    return cStringEntrada.length;
-}
+// function contagem3() {
+//     let cStringEntrada = "Eduardo Coelho";
+//     document.getElementById("cstring").innerHTML = cStringEntrada;
+//     return cStringEntrada.length;
+// }
 
-let resultado3 = contagem3();
+// let resultado3 = contagem3();
 
-document.getElementById("saida3").innerHTML = resultado3;
-console.log(resultado3);
+// document.getElementById("saida3").innerHTML = resultado3;
+// console.log(resultado3);
 
 
 // Exercício 4
-let dStringEntrada = "";
+// let dStringEntrada = "";
 
-function inversao() {
-    let dStringEntrada = "Eduardo Coelho";
-    document.getElementById("dstring").innerHTML = dStringEntrada;
-    return dStringEntrada.split("").reverse().join("");
-}
+// function inversao() {
+//     let dStringEntrada = "Eduardo Coelho";
+//     document.getElementById("dstring").innerHTML = dStringEntrada;
+//     return dStringEntrada.split("").reverse().join("");
+// }
 
-let resultado4 = inversao();
+// let resultado4 = inversao();
 
-document.getElementById("saida4").innerHTML = resultado4;
-console.log(resultado4);
+// document.getElementById("saida4").innerHTML = resultado4;
+// console.log(resultado4);
 
 
 // Exercício 5
-const elistaNumeros = [];
+// const elistaNumeros = [];
 
-function maiorNumero() {
-    const elistaNumeros = [1, 4, 6, 10, 3, 50];
-    document.getElementById("estring").innerHTML = elistaNumeros;
-    elistaNumeros.sort(function(a, b) {return b-a;});
-    return elistaNumeros[0];
-}
+// function maiorNumero() {
+//     const elistaNumeros = [1, 4, 6, 10, 3, 50];
+//     document.getElementById("estring").innerHTML = elistaNumeros;
+//     elistaNumeros.sort(function(a, b) {return b-a;});
+//     return elistaNumeros[0];
+// }
 
-let resultado5 = maiorNumero();
+// let resultado5 = maiorNumero();
 
-document.getElementById("saida5").innerHTML = resultado5;
+// document.getElementById("saida5").innerHTML = resultado5;
 
 
 // Exercício 6
-const flistaNumeros = [];
-let soma2 = 0;
+// const flistaNumeros = [];
+// let soma2 = 0;
 
-function mediaNumero(i) {
-    const flistaNumeros = [5, 10, 5, 5, 10, 10]
-    document.getElementById("fstring").innerHTML = flistaNumeros;
-    for(var i = 0; i < flistaNumeros.length; i++) {
-        soma2 += flistaNumeros[i];
-    }
+// function mediaNumero(i) {
+//     const flistaNumeros = [5, 10, 5, 5, 10, 10]
+//     document.getElementById("fstring").innerHTML = flistaNumeros;
+//     for(var i = 0; i < flistaNumeros.length; i++) {
+//         soma2 += flistaNumeros[i];
+//     }
 
-    return soma2 / (flistaNumeros.length);
-}
+//     return soma2 / (flistaNumeros.length);
+// }
 
-let resultado6 = mediaNumero();
+// let resultado6 = mediaNumero();
 
-const fArray = document.getElementById("farray");
+// const fArray = document.getElementById("farray");
 
-document.getElementById("saida6").innerHTML = resultado6;
+// document.getElementById("saida6").innerHTML = resultado6;
 
 
 // Exercício 7
-const pessoa = {};
+// const pessoa = {};
 
-function propriedades() {
-    const pessoa = {nome:"Eduardo", sobrenome: "Coelho", idade:"19", cidade:"Antônio Carlos"};
-    document.getElementById("gobjeto").innerHTML = pessoa.nome + " " + pessoa.sobrenome + " " + pessoa.idade + " " + pessoa.cidade;
-    let caracteristicas = Object.values(pessoa)
-    return caracteristicas.length
-}
-let resultado7 = propriedades();
+// function propriedades() {
+//     const pessoa = {nome:"Eduardo", sobrenome: "Coelho", idade:"19", cidade:"Antônio Carlos"};
+//     document.getElementById("gobjeto").innerHTML = pessoa.nome + " " + pessoa.sobrenome + " " + pessoa.idade + " " + pessoa.cidade;
+//     let caracteristicas = Object.values(pessoa)
+//     return caracteristicas.length
+// }
+// let resultado7 = propriedades();
 
-document.getElementById("saida7").innerHTML = resultado7;
+// document.getElementById("saida7").innerHTML = resultado7;
 
 
 // Exercício 8
-let hNum = 0;
+// let hNum = 0;
 
-function primo() {
-    let hNum = 11;
-    document.getElementById("hnum").innerHTML = hNum;
-    for (let i = 2; i < hNum; i++) {
-        if (hNum % i == 0) {
-                    return false;
-                } else {
-                    return true;
-                }
-    }
-}
+// function primo() {
+//     let hNum = 11;
+//     document.getElementById("hnum").innerHTML = hNum;
+//     for (let i = 2; i < hNum; i++) {
+//         if (hNum % i == 0) {
+//                     return false;
+//                 } else {
+//                     return true;
+//                 }
+//     }
+// }
 
-let resultado8 = primo();
+// let resultado8 = primo();
 
-document.getElementById("saida8").innerHTML = resultado8;
-console.log(resultado8);
+// document.getElementById("saida8").innerHTML = resultado8;
+// console.log(resultado8);
 
 // Exercício 9
-function corte() {
-    let iString = ["Eduardo", "19", "Coelho", "Casa"];
-    document.getElementById("iarray").innerHTML = iString;
-    let result = [];
-    for (let i of iString) {
-        if (i.length > 5) {
-            result.push(i);
-        }
-    }
-    return result.join(", ");
-}
+// function corte() {
+//     let iString = ["Eduardo", "19", "Coelho", "Casa"];
+//     document.getElementById("iarray").innerHTML = iString;
+//     let result = [];
+//     for (let i of iString) {
+//         if (i.length > 5) {
+//             result.push(i);
+//         }
+//     }
+//     return result.join(", ");
+// }
 
-document.getElementById("saida9").innerHTML = corte();
+// document.getElementById("saida9").innerHTML = corte();
 
 
 // Exercício 10
-function corte2() {
-    let jString = [2, 4, 5, 7, 10, 1];
-    document.getElementById("jarray").innerHTML = jString;
-    let result = [];
-    for (let j of jString) {
-        if (j % 2 == 0) {
-            result.push(j);
-        }
-    }
-    return result.join(", ");
-}
+// function corte2() {
+//     let jString = [2, 4, 5, 7, 10, 1];
+//     document.getElementById("jarray").innerHTML = jString;
+//     let result = [];
+//     for (let j of jString) {
+//         if (j % 2 == 0) {
+//             result.push(j);
+//         }
+//     }
+//     return result.join(", ");
+// }
 
-document.getElementById("saida10").innerHTML = corte2();
+// document.getElementById("saida10").innerHTML = corte2();
