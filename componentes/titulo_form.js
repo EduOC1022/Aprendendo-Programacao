ce3ecf79d152e11e4fef7278c9d1e3444b7e10ee
EduOC1022 <112728180+EduOC1022@users.noreply.github.com>
2023-08-01 15:14:14 -0300

    teste react

---
+++ componentes/titulo_form.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class titulo_descricao extends Component {
+    render() {
+        return (
+            <div id='titulo_descricao' class="texto">
+                <h1 id="titulo">Formulário de Teste</h1>
+                <p id="descricao">Formulário teste para primeiro contato com React</p>
+            </div>
+        )
+      }
+    }
+
+export default titulo_descricao;
